45e9a012c665212806b71ee0f928f509b88dec6c
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2023-10-16 09:20:28 +0330

    ADD: install requirement package and config project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+  plugins: [require("tailwindcss-flip"), require("daisyui")],
+  daisyui: {
+    themes: true,
+    darkTheme: "light",
+    base: true,
+    styled: true,
+    utils: true,
+    rtl: true,
+    prefix: "",
+    logs: true,
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
